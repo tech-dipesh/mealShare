3e9de03139a85177aae25be52d5d5ad9842d97ef
Dipesh <dipeshsharmagautam@outlook.com>
2025-08-06 10:08:21 +0530

    update the postcss

--- frontend/postcss.config.ts
+++ frontend/postcss.config.ts
@@ -1,6 +1,12 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import type { Config } from 'postcss-load-config'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+const config: Config = {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }
+
+export default config
